e5c16496b49d11ac28b0e0a14e133708808ca539
Jai Gupta <jai.g@supporthive.com>
2026-02-18 14:18:51 +0530

    Fix: Frontend view

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -6,9 +6,9 @@ class CallEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = CallEvent
         fields = [
-            'id', 'event_id', 'call_sid', 'account_sid', 'event_type',
+            'event_id', 'call_sid', 'account_sid', 'conference_sid', 'event_type',
             'call_status', 'direction', 'from_number', 'to_number',
-            'timestamp', 'created_at'
+            'timestamp'
         ]
 
 
@@ -16,7 +16,7 @@ class ErrorEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = ErrorEvent
         fields = [
-            'id', 'event_id', 'account_sid', 'correlation_sid',
-            'error_code', 'severity', 'product', 'http_status',
-            'webhook_url', 'error_message', 'request_sid', 'timestamp', 'created_at'
+            'event_id', 'account_sid', 'correlation_sid',
+            'error_code', 'severity', 'product',
+            'error_message', 'request_sid', 'timestamp'
         ]
